1bb7a6a2853d1b9255cceb942da917bed592d8b5
Jacob Adili <jadili@yahoo.com>
2025-06-02 14:13:29 -0500

    fixing webhook

--- app/javascript/src/success/index.jsx
+++ app/javascript/src/success/index.jsx
@@ -1,14 +1,14 @@
 // index.jsx
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Success from './success';
 
 document.addEventListener('DOMContentLoaded', () => {
   const node = document.getElementById('params');
   const data = JSON.parse(node.getAttribute('data-params'));
 
   ReactDOM.render(
-    <Property property_id={data.property_id} />,
-    document.body.appendChild(document.createElement('div')),
+    <Layout>
+      <h3>Booking Successful</h3>
+    </Layout>
   )
 })
