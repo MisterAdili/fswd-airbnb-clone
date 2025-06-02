58931beef70b9686c5c217853552d531bd1517f9
Jacob Adili <jadili@yahoo.com>
2025-06-02 14:53:03 -0500

    fixing webhook

--- app/javascript/src/success/index.jsx
+++ app/javascript/src/success/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Layout from '@src/layout';
 import Success from './success';
 
 document.addEventListener('DOMContentLoaded', () => {
 
   ReactDOM.render(
-    <Layout>
-      <Success />,
-      document.body.appendChild(document.createElement('div')),
-    </Layout>
+    <Success />,
+    document.body.appendChild(document.createElement('div')),
   )
 })
